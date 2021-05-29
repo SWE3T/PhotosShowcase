5575285048489ff240edc08bea47058575d6ee05
Guilherme <guilherme.devon@hotmail.com>
2021-05-29 17:28:03 -0300

    setting parallax

--- source/JavaScript.js
+++ source/JavaScript.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
-	// PARALLAX
-	$('.parallax').parallax();
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+  });
