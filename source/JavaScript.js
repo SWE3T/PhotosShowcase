f9b46e6c2c3e329e5821ed722722f7d41e850856
Guilherme <guilherme.devon@hotmail.com>
2021-05-31 13:36:07 -0300

    fixing grid

--- source/JavaScript.js
+++ source/JavaScript.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, 0);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, 0);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, 275);
+});
